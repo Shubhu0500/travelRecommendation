679b81337a81bb1b9e777dcb9b88023b4a9b80d8
Shubhu0500 <shubhu0500@gmail.com>
2024-11-16 17:15:18 +0530

    Changed alert in contact form

--- contactUs.js
+++ contactUs.js
@@ -2,5 +2,5 @@ const contactForm = document.getElementById("contact-form");
 
 contactForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  alert("Thank you for your message!");
+  alert("Thank you for your message");
 });
